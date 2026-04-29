ae9962c59ca666bba0aa451f31147724add6265c
DaDoBYNS <danielsee05@gmail.com>
2026-04-29 17:39:08 +0200

    validation_should_fail_when_a_ship_is_partially_outside_board

--- Core/RunAllTests/ShipsValidationTest.cpp
+++ Core/RunAllTests/ShipsValidationTest.cpp
@@ -17,4 +17,19 @@ TEST(ShipsValidation, validation_should_pass_when_all_ships_are_inside_board)
     
     Validation->ValidateShips(); 
     EXPECT_TRUE(Validation->GetIsValid());
+} 
+
+TEST(ShipsValidation, validation_should_fail_when_a_ship_is_partially_outside_board)
+{
+    std::shared_ptr<FValidation> Validation = std::make_shared<FValidation>(); 
+    std::shared_ptr<FBoard> Board = std::make_shared<FBoard>(); 
+    
+    Validation->SetBoard(Board); 
+    
+    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
+    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
+    Board->CreateShip(FPosition{ELetter::B, ENumber::Two}, 3);
+    
+    Validation->ValidateShips(); 
+    EXPECT_FALSE(Validation->GetIsValid());
 }
