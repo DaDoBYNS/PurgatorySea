eb859dfd1788547d632886d8a11f9e244ba30d40
DaDoBYNS <danielsee05@gmail.com>
2026-04-30 09:33:41 +0200

    validation_should_fail_when_two_ships_overlap

--- Core/PurgatorySeaCore/Validation.cpp
+++ Core/PurgatorySeaCore/Validation.cpp
@@ -6,6 +6,8 @@ FValidation::FValidation()
 
 void FValidation::ValidateShips()
 {
+    bool bTempIsValid = true; 
+    
     for (const auto& Ship : Board->GetShips())
     {
         for (const auto& Position : Ship->GetPositions())
@@ -13,14 +15,30 @@ void FValidation::ValidateShips()
             if (Position.Letter > Board->GetWidth()-1 || Position.Letter < 0
                 || Position.Number > Board->GetHeight()-1 || Position.Number < 0)
             {
-                bIsValid = false;
-                return;  
-            }    
+                bTempIsValid = false;
+            }
+            
+            for (const auto& CheckShip : Board->GetShips())
+            {
+                if (Ship == CheckShip)
+                {
+                    continue; 
+                }
+                
+                for (const auto& CheckPosition : CheckShip->GetPositions())
+                {
+                    if (CheckPosition.Letter == Position.Letter && CheckPosition.Number == Position.Number)
+                    {
+                        bTempIsValid = false; 
+                    }
+                }
+                
+            }
             
         }
     }
     
-    bIsValid = true; 
+    bIsValid = bTempIsValid; 
 }
 
 void FValidation::SetBoard(std::shared_ptr<FBoard> InBoard)
--- Core/RunAllTests/ShipsValidationTest.cpp
+++ Core/RunAllTests/ShipsValidationTest.cpp
@@ -12,8 +12,8 @@ TEST(ShipsValidation, validation_should_pass_when_all_ships_are_inside_board)
     Validation->SetBoard(Board); 
     
     Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
-    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
-    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
+    Board->CreateShip(FPosition{ELetter::C, ENumber::Four}, 3);
+    Board->CreateShip(FPosition{ELetter::D, ENumber::Four}, 3);
     
     Validation->ValidateShips(); 
     EXPECT_TRUE(Validation->GetIsValid());
@@ -26,10 +26,22 @@ TEST(ShipsValidation, validation_should_fail_when_a_ship_is_partially_outside_bo
     
     Validation->SetBoard(Board); 
     
-    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
-    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
     Board->CreateShip(FPosition{ELetter::B, ENumber::Two}, 3);
     
     Validation->ValidateShips(); 
     EXPECT_FALSE(Validation->GetIsValid());
+}
+
+TEST(ShipsValidation, validation_should_fail_when_two_ships_overlap)
+{
+    std::shared_ptr<FValidation> Validation = std::make_shared<FValidation>(); 
+    std::shared_ptr<FBoard> Board = std::make_shared<FBoard>(); 
+    
+    Validation->SetBoard(Board); 
+    
+    Board->CreateShip(FPosition{ELetter::B, ENumber::Four}, 3);
+    Board->CreateShip(FPosition{ELetter::B, ENumber::Six}, 5);
+    
+    Validation->ValidateShips(); 
+    EXPECT_FALSE(Validation->GetIsValid()); 
 }
